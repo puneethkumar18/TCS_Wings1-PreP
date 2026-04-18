d72f87b31a98725174a67af460b310c4256650b4
Puneeth G <2867750@tcs.com>
2026-04-18 12:30:48 +0530

    ORM- relationShips

--- modelDemo/empDemo/models.py
+++ modelDemo/empDemo/models.py
@@ -8,3 +8,37 @@ class Employee(models.Model):
     salary = models.FloatField()
     email = models.CharField(max_length=30)
     
+
+
+
+class Programmer(models.Model):
+    name = models.CharField(max_length=30)
+    salary = models.IntegerField()
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=30)
+    programeers = models.ManyToManyField(Programmer)
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=30)
+
+
+class PhoneNumbers(models.Model):
+    type = models.CharField(max_length=20)
+    number = models.CharField(max_length=12)
+    Customer = models.ForeignKey(Customer,on_delete=models.CASCADE)
+
+
+
+class Person(models.Model):
+    name = models.CharField(max_length=30)
+    age = models.IntegerField()
+
+
+class Licence(models.Model):
+    type  = models.CharField(max_length=30)
+    validfrom =  models.DateField()
+    validTo = models.DateField()
+    person = models.OneToOneField(Person,on_delete=models.CASCADE)
